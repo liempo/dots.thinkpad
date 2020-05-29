3d86241ed820eae9e7c619f7b3319f38994bb1d0
Liempo <gonzalesalec@gmail.com>
2020-05-29 14:42:16 +0800

    Screenlayout can run once per uptime

--- .screenlayout/dual.sh
+++ .screenlayout/dual.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 
+
 ### Fix error where DP-2 has no output yet has a signal
 
-# Turn off the external monitor 
-xrandr --output DP-2 --off
+if [ ! -f ~/.screenlayout/fixed ]; then
+    # Turn off the external monitor 
+    xrandr --output DP-2 --off
 
-# Wait for a few seconds
-sleep 1
+    # Wait for a few seconds
+    sleep 1
 
-# Load the actual config
-xrandr --output LVDS-1 --mode 1366x768 --pos 1920x312 --rotate normal --output DP-2 --primary --mode 1920x1080 --pos 0x0 --rotate normal
+    # Load the actual config
+    xrandr --output LVDS-1 --mode 1366x768 --pos 1920x312 --rotate normal --output DP-2 --primary --mode 1920x1080 --pos 0x0 --rotate normal
+    
+    # Create the file
+    touch ~/.screenlayout/fixed 
+fi
