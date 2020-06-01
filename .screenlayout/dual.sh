332a1a67a1e0f9f7dd77bc53cada5d8252fee4f3
Liempo <gonzalesalec@gmail.com>
2020-06-01 12:58:01 +0800

    Running display set up script with lightdm instead of bspwmrc

--- .screenlayout/dual.sh
+++ .screenlayout/dual.sh
@@ -3,16 +3,11 @@
 
 ### Fix error where DP-2 has no output yet has a signal
 
-if [ ! -f ~/.screenlayout/fixed ]; then
-    # Turn off the external monitor 
-    xrandr --output DP-2 --off
+# Turn off the external monitor 
+xrandr --output DP-2 --off
 
-    # Wait for a few seconds
-    sleep 1
+# Wait for a few seconds
+sleep 1
 
-    # Load the actual config
-    xrandr --output LVDS-1 --mode 1366x768 --pos 1920x312 --rotate normal --output DP-2 --primary --mode 1920x1080 --pos 0x0 --rotate normal
-    
-    # Create the file
-    touch ~/.screenlayout/fixed 
-fi
+# Load the actual config
+xrandr --output LVDS-1 --mode 1366x768 --pos 1920x312 --rotate normal --output DP-2 --primary --mode 1920x1080 --pos 0x0 --rotate normal
